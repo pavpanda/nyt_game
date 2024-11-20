11b3f7eece2809fd24295f4c214243437bef18d8
Pavan <pavan@dhcp-10-250-121-74.harvard.edu>
2024-11-20 08:57:26 -0500

    day 9

--- src/constants/gameConstants.ts
+++ src/constants/gameConstants.ts
@@ -2,32 +2,12 @@ import { Grid, TouchDragState } from '../types/types';
 
 export const LINK = "www.flipisfun.com";
 export const NUM_ROWS = 4;
-
-export const GAME_NUMBER = 8;
-
-export const NUMBER_TO_LETTER: { [key: number]: string } = {
-  1: 'E', 2: 'M', 3: 'I', 4: 'T',
-  5: 'I', 6: 'T', 7: 'E', 8: 'M',
-  9: 'M', 10: 'I', 11: 'T', 12: 'E',
-  13: 'T', 14: 'I', 15: 'M', 16: 'E'
-};
-
-export const SCRAMBLE = [
-  [11, 15, 1, 10],
-  [7, 12, 14, 3],
-  [2, 8, 5, 6],
-  [9, 16, 4, 13],
-];
-
-export const THEME = "Related, altered";
-
 export const SOLUTION: Grid = [
   [1, 2, 3, 4],
   [5, 6, 7, 8],
   [9, 10, 11, 12],
   [13, 14, 15, 16]
 ];
-
 export const initialTouchState: TouchDragState = {
   active: false,
   startX: 0,
@@ -38,3 +18,20 @@ export const initialTouchState: TouchDragState = {
   dragType: null,
   initialRect: null,
 };
+
+
+
+export const GAME_NUMBER = 9;
+export const NUMBER_TO_LETTER: { [key: number]: string } = {
+  1: 'K', 2: 'N', 3: 'E', 4: 'E',
+  5: 'L', 6: 'U', 7: 'N', 8: 'G',
+  9: 'N', 10: 'E', 11: 'C', 12: 'K',
+  13: 'N', 14: 'O', 15: 'S', 16: 'E'
+};
+export const SCRAMBLE = [
+  [9, 15, 4, 10],
+  [2, 12, 5, 3],
+  [7, 8, 14, 6],
+  [11, 16, 1, 13],
+];
+export const THEME = "Body Parts";
