918dc20c9ffad2625db2dcd334311597f93265c5
Pavan Pandurangi <55634930+pavpanda@users.noreply.github.com>
2024-11-26 08:00:30 -0600

    Update gameConstants.ts

--- src/constants/gameConstants.ts
+++ src/constants/gameConstants.ts
@@ -21,17 +21,17 @@ export const initialTouchState: TouchDragState = {
 
 
 
-export const GAME_NUMBER = 14;
+export const GAME_NUMBER = 15;
 export const NUMBER_TO_LETTER: { [key: number]: string } = {
-  1: 'C', 2: 'R', 3: 'A', 4: 'B',
-  5: 'F', 6: 'I', 7: 'S', 8: 'H',
-  9: 'R', 10: 'E', 11: 'E', 12: 'F',
-  13: 'S', 14: 'E', 15: 'A', 16: 'L'
+  1: 'C ', 2: 'L', 3: 'U', 4: 'B',
+  5: 'D', 6: 'E', 7: 'C', 8: 'K',
+  9: 'H', 10: 'A', 11: 'N', 12: 'D',
+  13: 'J', 14: 'A', 15: 'C', 16: 'K'
 };
 export const SCRAMBLE = [
-  [7, 11, 2, 16],
-  [5, 13, 10, 1],
-  [14, 4, 9, 6],
-  [12, 8, 3, 15],
+  [3, 15, 9, 12],
+  [7, 2, 13, 5],
+  [10, 14, 1, 16],
+  [8, 11, 6, 4],
 ];
-export const THEME = "In the ocean!";
+export const THEME = "We're playing poker. What do you see?";
