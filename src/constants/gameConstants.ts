91cc3573e03071266851148df45a8d12b2154d59
Pavan <pavan@Pavans-MacBook-Air.local>
2025-01-06 10:46:38 -0500

    day 55

--- src/constants/gameConstants.ts
+++ src/constants/gameConstants.ts
@@ -20,18 +20,18 @@ export const initialTouchState: TouchDragState = {
 };
 
 
-export const GAME_NUMBER = 54;
+export const GAME_NUMBER = 55;
 export const NUMBER_TO_LETTER: { [key: number]: string } = {
-  1: 'G', 2: 'O', 3: 'L', 4: 'D',
-  5: 'J', 6: 'A', 7: 'D', 8: 'E',
-  9: 'O', 10: 'P', 11: 'A', 12: 'L',
-  13: 'R', 14: 'U', 15: 'B', 16: 'Y'
+  1: 'B', 2: 'O', 3: 'A', 4: 'Z',
+  5: 'J', 6: 'O', 7: 'H', 8: 'N',
+  9: 'M', 10: 'A', 11: 'R', 12: 'Y',
+  13: 'N', 14: 'O', 15: 'A', 16: 'H'
 };
 export const SCRAMBLE = [
   [14, 13, 7, 1],
   [3, 4, 8, 16],
-  [10, 11, 2, 6],
-  [12, 9, 5, 15],
+  [10, 11, 5, 6],
+  [9, 12, 2, 15],
 ];
 
-export const THEME = "Precious materials";
+export const THEME = "Biblical Names";
