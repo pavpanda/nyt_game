c62998a7f80707a1d26627894ad5de2437585a80
Pavan <pavan@Pavans-MacBook-Air.local>
2025-01-23 18:13:53 -0600

    71

--- src/constants/gameConstants.ts
+++ src/constants/gameConstants.ts
@@ -20,12 +20,12 @@ export const initialTouchState: TouchDragState = {
 };
 
 
-export const GAME_NUMBER = 70;
+export const GAME_NUMBER = 71;
 export const NUMBER_TO_LETTER: { [key: number]: string } = {
-  1: 'B', 2: 'E', 3: 'S', 4: 'T',
-  5: 'FL', 6: 'AW', 7: 'LE', 8: 'SS',
-  9: 'I', 10: 'DE', 11: 'A', 12: 'L',
-  13: 'P', 14: 'ER', 15: 'FE', 16: 'CT'
+  1: 'CA', 2: 'ND', 3: 'I', 4: 'D',
+  5: 'HO', 6: 'N', 7: 'E', 8: 'ST',
+  9: 'S', 10: 'IN', 11: 'CE', 12: 'RE',
+  13: 'TR', 14: 'UT', 15: 'HF', 16: 'UL'
 };
 export const SCRAMBLE = [
   [2, 3, 6, 9],
@@ -34,4 +34,4 @@ export const SCRAMBLE = [
   [7, 13, 1, 15],
 ];
 
-export const THEME = "Awesome";
+export const THEME = "Genuine";
