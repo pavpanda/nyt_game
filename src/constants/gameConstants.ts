3f8a18a681953ba482a5436e6d1162e0a047d1dc
Pavan <pavan@dhcp-10-250-50-224.harvard.edu>
2025-01-30 11:43:57 -0500

    772

--- src/constants/gameConstants.ts
+++ src/constants/gameConstants.ts
@@ -25,7 +25,7 @@ export const NUMBER_TO_LETTER: { [key: number]: string } = {
   1: 'CH', 2: 'A', 3: 'L', 4: 'K',
   5: 'LE', 6: 'CT', 7: 'UR', 8: 'E',
   9: 'R', 10: 'O', 11: 'O', 12: 'M',
-  13: 'ST', 14: 'UD', 15: 'N', 16: 'T'
+  13: 'ST', 14: 'UD', 15: 'EN', 16: 'T'
 };
 export const SCRAMBLE = [
   [6, 10, 3, 9],
