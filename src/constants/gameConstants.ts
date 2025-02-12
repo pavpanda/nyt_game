796963530d7af3c5a562186d534f9ebabf7445ba
Pavan <pavan@dhcp-10-250-50-224.harvard.edu>
2025-02-12 18:01:25 -0500

    88

--- src/constants/gameConstants.ts
+++ src/constants/gameConstants.ts
@@ -20,20 +20,20 @@ export const initialTouchState: TouchDragState = {
 };
 
 
-export const GAME_NUMBER = 87;
+export const GAME_NUMBER = 88;
 export const NUMBER_TO_LETTER: { [key: number]: string } = {
-  1: 'F', 2: 'E', 3: 'E', 4: 'L',
-  5: 'H', 6: 'O', 7: 'L', 8: 'D',
-  9: 'M', 10: 'U', 11: 'S', 12: 'E',
-  13: 'W', 14: 'E', 15: 'E', 16: 'N'
+  1: 'E', 2: 'P', 3: 'I', 4: 'C',
+  5: 'M', 6: 'Y', 7: 'T', 8: 'H',
+  9: 'S', 10: 'A', 11: 'G', 12: 'E',
+  13: 'T', 14: 'A', 15: 'L', 16: 'E'
 };
 export const SCRAMBLE = [
   [6, 10, 3, 9],
   [11, 2, 16, 12],
   [8, 4, 5, 15],
-  [7, 14, 13, 1],
+  [7, 14, 1, 13],
 ];
 
-export const THEME = "Think";
+export const THEME = "Story";
 
-// hold feel ween muse
+//epic myth sage tale  
